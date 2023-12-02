b805a2daab42623520a17f39ec2db2be834a2085
Stephanie CP <stephers.cp@gmail.com>
2023-12-02 10:32:27 -0800

    Added instructions

---
+++ codecademy/codeChallenges/maxProfitDays.js
@@ -0,0 +1,21 @@
+// Maximize Stock Trading Profit
+// Challenge URL: https://www.codecademy.com/code-challenges/code-challenge-maximize-stock-trading-profit-javascript
+
+// Given the daily values of a stock, create a function called maxProfitDays() that, given a list of integers, will return the index value of the two elements that represent the day on which one should have bought a share and the day on which one should have sold a share based on the max profit.
+
+// A list of integers will represent the stock price at the beginning or “opening bell” of each day for a week. You are required to buy and sell only once. You also must buy a stock before selling it.
+
+// For example, given the list [17, 11, 60, 25, 150, 75, 31, 120], you can assume that index 0 represents day 0 and index 7 represents day 7. In this case, purchasing on day 1 and selling on day 4 would yield the most profit. If we were to call maxProfitDays([17, 11, 60, 25, 150, 75, 31, 120]), the function would return [1, 4].
+
+//RUNNING INSTRUCTIONS
+// Open terminal, navigate to: ...js-practice/Codecademy/codeChallenges
+// node maxProfitDays.js
+// Back up to parent with cd ../
+
+function maxProfitDays(stockPrices) {
+
+
+}
+
+// // Leave this so we can test your code:
+// module.exports = maxProfitDays;
--- codecademy/codeChallenges/statsFinder.js
+++ codecademy/codeChallenges/statsFinder.js
@@ -1,4 +1,9 @@
+// Calculate the Mean and Mode
 // Challenge URL: https://www.codecademy.com/code-challenges/code-challenge-calculate-the-mean-and-mode-javascript
+
+// Create a statsFinder() function that takes in a list of numbers and returns a list containing the mean and mode, in that order. As a reminder, the mean is the average of the values and the mode is the most occurring value. If there are multiple modes, return the mode that occurs first. Make sure that you write your functions and find these answers from scratch – don’t use imported tools!
+
+// For example, statsFinder([500, 400, 400, 375, 300, 350, 325, 300]) should return [368.75, 400].
 
 //RUNNING INSTRUCTIONS
 // Open terminal, navigate to: ...js-practice/Codecademy/codeChallenges
